307cc6c00fad2fe8a21e79f1c0f8c338eee4c917
orehunt <basso.bassista@gmail.com>
2021-01-05 07:10:33 +0100

    refactoring

--- user_data/modules/activation.py
+++ user_data/modules/activation.py
@@ -1,4 +1,5 @@
 import numpy as np
+from enum import IntEnum
 from numba import jit, jit_module
 
 
@@ -19,5 +20,40 @@ def softmax(X):
     expo_sum = np.sum(np.exp(X))
     return expo / expo_sum
 
+
+class FnType(IntEnum):
+    tanh = 0
+    relu = 1
+    sigmoid = 2
+    softmax = 3
+
+
+fn = (tanh, relu, sigmoid, softmax)
+
+
+def AND(x, y):
+    return x & y
+
+
+def OR(x, y):
+    return x | y
+
+
+def ABOVE(x, y):
+    return x > y
+
+
+def BELOWEQ(x, y):
+    return x <= y
+
+
+class OpType(IntEnum):
+    AND = 0
+    OR = 1
+    ABOVE = 2
+    BELOWEQ = 3
+
+
+op = (AND, OR, ABOVE, BELOWEQ)
 
 jit_module(nopython=True, error_model="numpy")
