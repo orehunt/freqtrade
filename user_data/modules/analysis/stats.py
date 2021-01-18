stats fixes, and intra class corr formula in numpy

user_data/modules/analysis/stats.py
@@ -31,25 +31,82 @@ def theils_u(x, y):
         return (s_x - s_xy) / s_x
 
 
+# also check:
+# - intra class correlation
+# - cron bach reliability score
+# - scipy.stats.sem (standard error of measure)
 def correlation_ratio(categories, measurements):
     fcat, _ = pd.factorize(categories)
     cat_num = np.max(fcat) + 1
     y_avg_array = np.zeros(cat_num)
     n_array = np.zeros(cat_num)
     for i in range(0, cat_num):
-        cat_measures = measurements[np.argwhere(fcat == i).flatten()]
+        cat_measures = measurements[:, np.argwhere(fcat == i).flatten()]
         n_array[i] = len(cat_measures)
-        y_avg_array[i] = np.average(cat_measures)
-    y_total_avg = np.sum(np.multiply(y_avg_array, n_array)) / np.sum(n_array)
-    numerator = np.sum(
+        y_avg_array[i] = np.nanmean(cat_measures)
+    y_total_avg = np.nansum(np.multiply(y_avg_array, n_array)) / np.nansum(n_array)
+    numerator = np.nansum(
         np.multiply(n_array, np.power(np.subtract(y_avg_array, y_total_avg), 2))
     )
-    denominator = np.sum(np.power(np.subtract(measurements, y_total_avg), 2))
+    denominator = np.nansum(np.power(np.subtract(measurements, y_total_avg), 2))
     if numerator == 0:
         eta = 0.0
     else:
         eta = np.sqrt(numerator / denominator)
     return eta
+
+
+def icc(data, targets, normalize=True, zero_fill=False):
+    """
+    :data: ndarray, each column represent the sample of observations for a specific metric
+    :targets: ndarray, each column represent one element of the group for correlating the metrics
+    A group is composed of one column from data, with targets, eg.
+    (concat([data[:, n], targets], axis=1) for n in data.shape[0])
+    """
+    N = data.shape[0]
+    assert N == targets.shape[0]
+
+    targets = targets.values
+    data = data.values
+    if zero_fill:
+        targets[np.isnan(targets)] = 0
+        data[np.isnan(data)] = 0
+
+    if len(targets.shape) == 1:
+        K = 2
+        targets = targets[:, None]
+    else:
+        K = targets.shape[1] + 1
+    if len(data.shape) == 1:
+        data = data[:, None]
+    if zero_fill:
+        targets_sum = targets.sum(axis=1)[:, None]
+    else:
+        targets_sum = np.nansum(targets, axis=1)[:, None]
+
+    groups_sum = data + targets_sum
+    d = 1 / K * N
+    total_mean = d * groups_sum.sum(axis=0)
+
+    member_diff = np.empty((targets.shape[0], targets.shape[1], total_mean.shape[0]))
+    for t in range(K - 1):
+        member_diff[:, t, :] = targets[:, t][:, None] - total_mean
+
+    if zero_fill:
+        targets_E = np.power(member_diff, 2).sum(axis=0).sum(axis=0)
+        data_E = np.power(data - total_mean, 2).sum(axis=0)
+    else:
+        targets_E = np.nansum(np.nansum(np.power(member_diff, 2), axis=0), axis=0)
+        data_E = np.nansum(np.power(data - total_mean, 2), axis=0)
+
+    s2 = d * (targets_E + data_E)
+    r = K / (K - 1) * (
+        (np.power(groups_sum.mean(axis=0) - total_mean, 2).sum(axis=0) / N) / s2
+    ) - (1 * K - 1)
+
+    if normalize:
+        r = (r - r.min()) / (r.max() / r.min())
+    return r
 
 
 def find_correlation(df, thresh=0.9):